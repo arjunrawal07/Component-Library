903cfc2eb014ae6ef6ddeee1c717a9531423a735
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-04-05 20:22:35 -0400

    voucher buttons

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,8 +3,21 @@ import "./Form.css";
 
 const Form = (props) => {
   let classList = "";
-
-  let types = ["default"];
+  let imgClass = "";
+  let placeText = "";
+  let redeemClass = "";
+  let buttonLabel = "";
+  let count = "0";
+  let types = ["default", "select-default", "number"];
+  let placeholderValues = ["Select", "Email", "Voucher code"];
+  let buttonTexts = ["Redeem"];
+  let test = "";
+  const callBack = (event) => {
+    test += event.target.value;
+    if (event.target.value !== test) {
+      console.log("Submitted!");
+    }
+  };
 
   if (types.includes(props.type)) {
     classList += ` form-${props.type}`;
@@ -17,10 +30,68 @@ const Form = (props) => {
   if (props.large) {
     classList += ` form-large`;
   }
+  if (props.dropDown) {
+    imgClass += ` form-dropdown`;
+  }
+
+  if (placeholderValues.includes(props.placeholder)) {
+    placeText += ` ${props.placeholder}`;
+  }
+
+  if (props.selectMedium) {
+    classList += ` form-select-medium`;
+  }
+
+  if (props.selectLarge) {
+    classList += ` form-select-large`;
+  }
+
+  if (props.filled) {
+    classList += ` form-select-fill`;
+  }
+
+  if (props.number) {
+    classList += ` form-number`;
+    return (
+      <div>
+        <button onClick={count - 1}>-</button>
+        <div>{count}</div>
+        <button onClick={count + 1}>+</button>
+      </div>
+    );
+  }
+  if (buttonTexts.includes(props.buttontext)) {
+    buttonLabel += ` ${props.buttontext}`;
+  }
+  if (props.redeemMedium) {
+    redeemClass += ` form-redeemMedium`;
+    return (
+      <div>
+        <form onSubmit={callBack}>
+          <input className={classList} type="text" placeholder={placeText} />
+          <input type="submit" className={redeemClass} value={buttonLabel} />
+          {/* <button className={redeemClass}>{buttonLabel}</button> */}
+        </form>
+      </div>
+    );
+  }
+  if (props.redeemLarge) {
+    redeemClass += ` form-redeemLarge`;
+    return (
+      <div>
+        <form onSubmit={callBack}>
+          <input className={classList} type="text" placeholder={placeText} />
+          <input type="submit" className={redeemClass} value={buttonLabel} />
+          {/* /* <button className={redeemClass}>{buttonLabel}</button> */}
+        </form>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <p>Email</p>
-      <input className={classList} type="text" placeholder="  Email" />
+      <input className={classList} type="text" placeholder={placeText} />
+      <div className={imgClass}></div>
     </div>
   );
 };
--- src/components/Form/Form.stories.js
+++ src/components/Form/Form.stories.js
@@ -4,12 +4,116 @@ import { storiesOf } from "@storybook/react";
 import Form from "./Form";
 
 storiesOf("Form", module)
-  .add("Default Email", () => <Form label="Email" type="default" />)
+  .add("Default Email", () => (
+    <div className="header">
+      Email
+      <Form label="Email" placeholder="Email" type="default" />
+    </div>
+  ))
 
   .add("Default Email Medium", () => (
-    <Form label="Email" type="default" medium />
+    <div className="header">
+      Email
+      <Form label="Email" type="default" placeholder="Email" medium />
+    </div>
   ))
 
   .add("Default Email Large", () => (
-    <Form label="Email" type="default" large />
+    <div className="header">
+      Email
+      <Form label="Email" type="default" placeholder="Email" large />
+    </div>
+  ))
+
+  .add("Select Input Default", () => (
+    <div className="header">
+      Select
+      <Form label="Select" type="select-default" placeholder="Select" />
+    </div>
+  ))
+
+  .add("Select Input Medium", () => (
+    <div className="header">
+      Select
+      <Form
+        label="Select"
+        type="select-default"
+        placeholder="Select"
+        selectMedium
+      />
+    </div>
+  ))
+
+  .add("Select Input Large", () => (
+    <div className="header">
+      Select
+      <Form
+        label="Select"
+        type="select-default"
+        placeholder="Select"
+        selectLarge
+      />
+    </div>
+  ))
+
+  .add("Select Input Default Filled", () => (
+    <div className="header">
+      Select
+      <Form label="Select" type="select-default" placeholder="Select" filled />
+    </div>
+  ))
+
+  .add("Select Input Medium Filled", () => (
+    <div className="header">
+      Select
+      <Form
+        label="Select"
+        type="select-default"
+        placeholder="Select"
+        selectMedium
+        filled
+      />
+    </div>
+  ))
+
+  .add("Select Input Large Filled", () => (
+    <div className="header">
+      Select
+      <Form
+        label="Select"
+        type="select-default"
+        placeholder="Select"
+        selectLarge
+        filled
+      />
+    </div>
+  ))
+
+  .add("Number Input", () => (
+    <div>
+      <Form label="Counter" type="number" number />
+    </div>
+  ))
+
+  .add("Voucher Button Medium", () => (
+    <div className="header">
+      <Form
+        type="default"
+        placeholder="Voucher code"
+        buttontext="Redeem"
+        medium
+        redeemMedium
+      />
+    </div>
+  ))
+  .add("Voucher Button Large", () => (
+    <div className="header">
+      <Form
+        type="default"
+        placeholder="Voucher code"
+        buttontext="Redeem"
+        large
+        redeemLarge
+      />
+    </div>
   ));

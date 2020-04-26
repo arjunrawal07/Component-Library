3bced2bc4712062235ccb8c37c9e6333b9983fe6
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-04-26 19:06:25 -0400

    resubmission

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,28 +5,8 @@ const Form = (props) => {
   let classList = "";
   let imgClass = "";
   let placeText = "";
-  let redeemClass = "";
-  let buttonLabel = "";
-  let count = "0";
-  let types = ["default", "select-default", "number", "checkbox"];
-  let placeholderValues = ["Select", "Email", "Voucher code"];
-  let buttonTexts = ["Redeem"];
-  let test = "";
-
-  const callBack = (event) => {
-    event.preventDefault();
-    test += event.target.value;
-    if (event.target.value !== test) {
-      console.log("Submitted!");
-    }
-  };
-  const decrement = ({ count }) => {
-    return (count -= 1);
-  };
-
-  const increment = ({ count }) => {
-    return (count += 1);
-  };
+  let types = "default";
+  let placeholderValues = "Email";
 
   if (types.includes(props.type)) {
     classList += ` form-${props.type}`;
@@ -45,18 +25,6 @@ const Form = (props) => {
 
   if (placeholderValues.includes(props.placeholder)) {
     placeText += ` ${props.placeholder}`;
-  }
-
-  if (props.selectMedium) {
-    classList += ` form-select-medium`;
-  }
-
-  if (props.selectLarge) {
-    classList += ` form-select-large`;
-  }
-
-  if (props.filled) {
-    classList += ` form-select-fill`;
   }
 
   return (

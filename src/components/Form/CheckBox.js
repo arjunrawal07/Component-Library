1d07dbd6d16fee4c3af269571b6f5400741d99ce
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-04-29 23:57:48 -0400

    fixed first two checkboxes

--- src/components/Form/CheckBox.js
+++ src/components/Form/CheckBox.js
@@ -3,39 +3,21 @@ import "./Form.css";
 
 const Checkbox = (props) => {
   let classList = "";
-  let imgClass = "";
-  let placeText = "";
-  let types = "checkbox";
 
-  if (types.includes(props.type)) {
-    classList += ` form-${props.type}`;
-  }
-  if (props.blue) {
-    classList += ` form-blue`;
-  }
-  if (props.popup) {
-    classList += ` form-popup`;
-    return (
-      <div>
-        <div className="blackCheck"></div>
-        <div className="text">
-          <p>Don't show this popup again</p>
-        </div>
-      </div>
-    );
-  }
-  if (props.checkedState) {
-    return <div className="blackCheck"></div>;
+  if (props.color == "black") {
+    classList += "black";
   }
 
-  if (props.checkedStateBlue) {
-    return <div className="blueCheck"></div>;
+  if (props.color == "blue") {
+    classList += "blue";
   }
+
   return (
-    <div>
-      <input className={classList} type="text" placeholder={placeText} />
-      <div className={imgClass}></div>
-    </div>
+    <label id="check" className={classList}>
+      <input type="checkbox" className="checkaroo"></input>
+      <span className="custom"></span>
+      <span className="Label">{props.label}</span>
+    </label>
   );
 };
 
--- src/components/Form/CheckBox.stories.js
+++ src/components/Form/CheckBox.stories.js
@@ -5,28 +5,18 @@ import Checkbox from "./CheckBox";
 storiesOf("Checkboxes", module)
   .add("Empty Black Checkbox", () => (
     <div>
-      <Checkbox type="checkbox" />
-    </div>
-  ))
-  .add("Checked Black Checkbox", () => (
-    <div>
-      <Checkbox type="checkbox" checkedState />
+      <Checkbox color="black" />
     </div>
   ))
 
   .add("Empty Blue Checkbox", () => (
     <div>
-      <Checkbox type="checkbox" blue />
+      <Checkbox color="blue" />
     </div>
   ))
 
-  .add("Checked Blue Checkbox", () => (
-    <div>
-      <Checkbox type="checkbox" checkedStateBlue />
-    </div>
-  ))
   .add("No Popups", () => (
     <div>
-      <Checkbox type="checkbox" checkedState popup />
+      <Checkbox color="black" label="Don't show this popup again"></Checkbox>
     </div>
   ));
